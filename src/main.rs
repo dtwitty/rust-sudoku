c3f51fc10cbf4d3d4b156fd82917a7509f9210f5
Dominick Twitty <dktwitty@gmail.com>
2022-01-10 13:45:41 -0800

    More comments

--- src/main.rs
+++ src/main.rs
@@ -231,24 +231,39 @@ impl fmt::Display for Board {
     }
 }
 
+// This function finds the first CandidateSet with a single set bit.
+// It is the meat-and-potatoes of the solver and should be 
+// AS FAST AS POSSIBLE!!!
 fn single_candidate_position(data: &[CandidateSet]) -> Option<usize> {
     const N: usize = 128;
+    // Chunk the input array so that many positions are considered at once.
     data.chunks(N).enumerate().find_map(|(chunk, s)| {
+        // Create a 32-byte aligned array of u8s where each position encodes 'Is Single Bit'.
+        // It is faster to compute "Is Single Bit" all at once, then find the first in a second pass.
         let mut a: Aligned<A32, _> = Aligned([0u8; N]);
         let arr = a.as_mut_slice();
         arr.iter_mut()
             .zip(s.iter())
+            // Calculate whether this position has a single bit.
             .for_each(|(a, &c)| *a = (((c != 0) & ((c & (c - 1)) == 0)) as u8) * 0xFF);
+        
+        // Now that we have an array of "has_single_bit" flags, we need to find the first (if any).
+        // The idea is to avoid branching at all costs! We populate a u128 where each bit position
+        // encodes 'has_single_bit' in the input array.
         unsafe {
             let ptr = arr.as_ptr();
 
             let mut bitmask: u128 = 0;
             for i in 0..N / 32 {
+                // Load 32 bytes of 0 or 0xFF.
                 let packed_bits = _mm256_load_si256(ptr.offset(i as isize * 32) as *const __m256i);
+                // Pack the first bit of each into a u32, then add that to a u128.
                 let bits = _mm256_movemask_epi8(packed_bits) as u128;
                 bitmask |= bits << (i * 32);
             }
 
+            // This is the only branch needed to check a whole chunk of positions.
+            // We simply check if our special bit array is non-zero, and find the first bit set.
             if bitmask != 0 {
                 Some(chunk * N + (bitmask.trailing_zeros() as usize))
             } else {
@@ -258,6 +273,7 @@ fn single_candidate_position(data: &[CandidateSet]) -> Option<usize> {
     })
 }
 
+// These are the possible outcomes of constraint propagation.
 #[derive(PartialEq)]
 enum ConstraintPropagationResult {
     NoConstraintsPropagated,
@@ -269,19 +285,29 @@ enum ConstraintPropagationResult {
 impl Board {
     fn new() -> Board {
         Board {
+            // Which values are candidates for this cell?
             candidates: [ALL_CANDS; 81],
+            
+            // What is the solved value at this cell?
             values: [NO_VALUE; 81],
+            
+            // How many cells are left to solve?
             num_remaining_cells: 81,
+            
+            // Where is each value available in each group?
+            // For example, we can ask 'In Row 2, which cells can take a 5?'
             candidate_to_groups: CandidateToGroups {
                 candidates: [ALL_CANDS; 9 * 3 * 9],
             },
         }
     }
 
+    // Is this board solved?
     fn is_solved(&self) -> bool {
         self.num_remaining_cells == 0
     }
 
+    // Is every cell set, and do its neighbors obey the Sudoku rules?
     fn is_complete(&self) -> bool {
         (0..81).all(|idx| {
             self.values[idx].is_set()
@@ -291,13 +317,15 @@ impl Board {
         })
     }
 
+    // Get mutable candidates at a cell, assuming a valid index.
     fn unsafe_mut_candidates_at(&mut self, idx: CellIdx) -> &mut CandidateSet {
         unsafe {
             core::intrinsics::assume(idx < 81);
         }
         &mut self.candidates[idx]
     }
-
+    
+    // Get candidates at a cell, assuming a valid index.
     fn unsafe_candidates_at(&self, idx: CellIdx) -> &CandidateSet {
         unsafe {
             core::intrinsics::assume(idx < 81);
@@ -305,19 +333,25 @@ impl Board {
         &self.candidates[idx]
     }
 
+    // Set a value at a cell.
+    // This function handles updating the constraint propagation data structures.
     #[inline(never)]
     fn set_value_at(&mut self, idx: CellIdx, v: Value) {
         unsafe {
             core::intrinsics::assume(idx < 81);
         }
+        
+        // Basic bookkeeping.
         self.values[idx] = v;
         self.candidates[idx] = 0;
         self.num_remaining_cells -= 1;
 
+        // Erase this value from the cell's neighbors.
         all_neighbors(idx).iter().for_each(|&other_idx| {
             self.unsafe_mut_candidates_at(other_idx).remove_candidate(v);
         });
 
+        // Get the row, col, and box for this cell.
         let r = Row::for_cell(idx);
         let c = Col::for_cell(idx);
         let b = Box::for_cell(idx);
@@ -328,103 +362,150 @@ impl Board {
             core::intrinsics::assume(b < 9);
         }
 
+        // For each candidate...
         for i in 0..9 {
             self.candidate_to_groups
                 .mut_groups_for_candidate(i)
+                // Update the row of the cell...
                 .mut_row_candidates(r)
+                // To say that this cell is no longer available.
                 .remove_candidate(Row::group_idx(idx) as Value);
         }
+        
+        // For each candidate..
         for i in 0..9 {
             self.candidate_to_groups
                 .mut_groups_for_candidate(i)
+                // Update the col of the cell...
                 .mut_col_candidates(c)
+                // To say that this cell is no longer available.
                 .remove_candidate(Col::group_idx(idx) as Value);
         }
+        
+        // For each candidate...
         for i in 0..9 {
             self.candidate_to_groups
                 .mut_groups_for_candidate(i)
+                // Update the box of the cell...
                 .mut_box_candidates(b)
+                // To say that this cell is no longer available.
                 .remove_candidate(Box::group_idx(idx) as Value);
         }
 
+        // This value is no longer available anywhere in the current row.
         *self
             .candidate_to_groups
             .mut_groups_for_candidate(v)
             .mut_row_candidates(r) = 0;
+        
+        // This value is no longer available anywhere in the current col.
         *self
             .candidate_to_groups
             .mut_groups_for_candidate(v)
             .mut_col_candidates(c) = 0;
+        
+        // This value is no longer available anywhere in the current box.
         *self
             .candidate_to_groups
             .mut_groups_for_candidate(v)
             .mut_box_candidates(b) = 0;
 
+        // For each row...
         for r in 0..9 {
             self.candidate_to_groups
+                // The current value...
                 .mut_groups_for_candidate(v)
                 .mut_row_candidates(r)
+                // Is not available in the current column.
                 .remove_candidate(c as Value);
         }
 
+        // For each box...
         for br in 0..3 {
             for bc in 0..3 {
                 let r = (b / 3) * 3 + br;
                 let c = (b % 3) * 3 + bc;
                 self.candidate_to_groups
+                    // The current value...
                     .mut_groups_for_candidate(v)
                     .mut_row_candidates(r)
+                    // Is not available in the current colum.
                     .remove_candidate(c as Value);
             }
         }
 
+        // For each column...
         for c in 0..9 {
             self.candidate_to_groups
+                // The current value...
                 .mut_groups_for_candidate(v)
                 .mut_col_candidates(c)
+                // Is not available in the current row.
                 .remove_candidate(r as Value);
         }
 
+        // For each box...
         for bc in 0..3 {
             for br in 0..3 {
                 let r = (b / 3) * 3 + br;
                 let c = (b % 3) * 3 + bc;
                 self.candidate_to_groups
+                    // The current value
                     .mut_groups_for_candidate(v)
                     .mut_col_candidates(c)
+                    // Is not available in the current row.
                     .remove_candidate(r as Value);
             }
         }
 
+        // For each column...
         for c in 0..9 {
             self.candidate_to_groups
+                // The current value...
                 .mut_groups_for_candidate(v)
                 .mut_box_candidates((r / 3) * 3 + (c / 3))
+                // Is not available in the current box.
                 .remove_candidate(((r % 3) * 3 + (c % 3)) as Value);
         }
 
+        // For each row...
         for r in 0..9 {
             self.candidate_to_groups
+                // The current value...
                 .mut_groups_for_candidate(v)
                 .mut_box_candidates((r / 3) * 3 + (c / 3))
+                // Is not available in the current box.
                 .remove_candidate(((r % 3) * 3 + (c % 3)) as Value);
         }
     }
 
+    // This function identifies the cell with the most constraints.
+    // This is useful because this is the easiest cell to exhaustively check with backtracking.
     #[inline(never)]
     fn most_constrained_cell(&self) -> CellIdx {
+        // An array of [i % 3 for i in range(81)]
+        // This is needed because integer modulus is not provided by SIMD instructions.
         const MODS: [u8; 81] = [
             0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1,
             2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0,
             1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2,
         ];
 
+        // Counts bits set in a u16.
+        // This is needed because popcnt is not a vectorized instruction.
         fn cbs(mut v: u16) -> u16 {
             v = v - ((v >> 1) & 0x5555);
             v = (v & 0x3333) + ((v >> 2) & 0x3333);
             ((v + (v >> 4) & 0xF0F) * 0x101) >> 8
         }
 
+        // This array holds values of the following format for each cell
+        // <has_zero_candidates> (1 bit) - to toss out finsihed cells
+        // <num_candidates> (4 bits, with one extra) - to prefer cells with fewer candidates
+        // <cell_id % 3> (2 bits)
+        // <cell_id % 3> is a poor-man's randomization. Without it, the solver will prefer cells
+        // near the end of the array, leading to bunched backtracking that clears fewer candidates.
+        // With it, earlier cells may trump later ones, leading to more even candidate clearing.
         let mut arr = [0u8; 81];
         arr.iter_mut()
             .zip(self.candidates.iter())
@@ -435,6 +516,9 @@ impl Board {
                 let n = cbs(c as u16) as u8;
                 *a = is_zero << 7 | n << 2 | m;
             });
+        
+        // Select the cell_id with candidates the lowest number of candidates,
+        // following constraints above.
         (arr.iter()
             .enumerate()
             .map(|(i, &a)| (i as u8, a))
@@ -443,6 +527,8 @@ impl Board {
             .0) as usize
     }
 
+    // This function repeatedly propagates constraints until it can no longer make any
+    // more inferences about the board.
     fn propagate_constraints(&mut self) -> ConstraintPropagationResult {
         let mut result = ConstraintPropagationResult::PropagatedConstraint;
         while result == ConstraintPropagationResult::PropagatedConstraint {
@@ -451,6 +537,7 @@ impl Board {
         result
     }
 
+    // This function attempts to propagate a constraint, or assert that the board is solved or unsolveable.
     fn try_propagate_constraints(&mut self) -> ConstraintPropagationResult {
         if self.is_solved() {
             return ConstraintPropagationResult::Solved;
@@ -467,11 +554,18 @@ impl Board {
         ConstraintPropagationResult::NoConstraintsPropagated
     }
 
+    // This function detects whether the board has any cells that can't be satisfied.
+    // This means there is some cell that doesn't have a set value and has no
+    // remaining candidate values.
     #[inline(never)]
     fn has_conflict(&self) -> bool {
         const N: usize = 64;
+        
+        // The idea is to process big chunks of the values and candidates array looking for conflicts.
         return self
             .candidates
+            // We use chunking to give the compiler convenient array boundaries to add branch points.
+            // This encourages the compiler to do each chunk in SIMD, then do a single branch for each chunk.
             .chunks(N)
             .zip(self.values.chunks(N))
             .any(|(c_chunk, v_chunk)| {
@@ -484,8 +578,12 @@ impl Board {
             });
     }
 
+    // This function finds and sets a 'naked single' if one exists.
+    // A naked single is a cell that only has one candidate value.
     #[inline(never)]
     fn set_naked_single(&mut self) -> bool {
+        // The candidates array contains a bitset of candidates for every position.
+        // If any of these bitsets contains a single bit, it must be a naked single!
         let i = single_candidate_position(&self.candidates);
 
         if let Some(idx) = i {
@@ -498,10 +596,17 @@ impl Board {
         false
     }
 
+    // This function finds and sets a 'hidden single' if one exists.
+    // A hidden single is a value that is only viable at one cell in a group.
     #[inline(never)]
     fn set_hidden_singles(&mut self) -> bool {
+        // The candidate_to_groups_mapping tells where each value is viable in each group.
+        // If there are any single-bit values in that array, that is a hidden single!
         let o = single_candidate_position(&self.candidate_to_groups.candidates);
+        
         if let Some(i) = o {
+            // We found a hidden single! We just need to extract its position information so
+            // we can set the value at the right cell.
             let group_idx = i % 9;
             let group_type = (i / 9) % 3;
             let v = i / 9 / 3;
@@ -521,6 +626,8 @@ impl Board {
             self.set_value_at(idx, v as Value);
             return true;
         }
+        
+        // No hidden singles found :(
         false
     }
 }
@@ -528,21 +635,34 @@ impl Board {
 pub fn solve(board: &mut Board) -> Option<Board> {
     let res = board.propagate_constraints();
     match res {
+        // If the board is solved, return it!
         ConstraintPropagationResult::Solved => Some(*board),
+        
+        // If we found a conflict, we reject this board.
         ConstraintPropagationResult::FoundConflict => None,
+        
+        // We don't know if this board is on the right path, so we have to backtrack.
         _ => {
+            // Pick the most constrained cell.
             let idx = board.most_constrained_cell();
+            
+            // Check every possible candidate for that cell.
             for v in 0..9 {
                 if board.unsafe_candidates_at(idx).has_candidate(v) {
+                    // Copy the current board, and set the value in the new board.
                     let mut new_board: Board = *board;
                     new_board.set_value_at(idx, v);
 
+                    // Check whether the new board is now solveable.
                     let solution = solve(&mut new_board);
                     if solution.is_some() {
                         return solution;
                     }
                 }
             }
+            
+            // We have checked every candidate for a given cell and found no suitable value.
+            // This board must be unsolveable.
             None
         }
     }
